70e775040082b495256b5e061c8300b6db4a1707
c.barbone <camille.barbone@hotmail.com>
2021-03-17 17:09:40 +0100

    ajout ban particules + page com administrable

--- wp-content/themes/base/template_pageInternet.php
+++ wp-content/themes/base/template_pageInternet.php
@@ -2,126 +2,39 @@
 
 <?php get_header(); ?>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-12 d-flex justify-content-center">
                 <h2 class="titrepage">Site internet</h2>
             </div>
-        
-            <div class="col-4">
-                <h3 class="titresite">Système de gestion de contenu (cms)</h3>
-                <p>WordPress vous offre la possibilité de vous connecter afin de modifier votre contenu
-                    en quelques clics seulement !</p>
-                <h3 class="titresite">Responsiv Design</h3>
-                <p>Nous adaptons votre site web aux appareils 
-                    mobiles, tablettes, ainsi qu’aux ordinateurs
-                    portables et téléviseurs.</p>
-                <h3 class="titresite">Modules Standards</h3>
-                <p>Installation de modules standards : 
-                    Formulaire de contact, carte Google Maps, 
-                    Newsletter…</p>
-                <h3 class="titresite">Web Design et configuration du site</h3>
-                <p>Nous nous occupons de l’installation et la 
-                    configuration du CMS, des modules et de la 
-                    mise en place d’adresse(s) mail.</p>
+            <div class="col-lg-4 col-sm-12 text-center">
+                <?php $lignechrono1 = get_field("ligne_chronologique1");
+                    for ($i=0; $i < count($lignechrono1); $i++){
+                    echo '<img class="logochrono1" src="'.$lignechrono1[$i]["logo_chrono1"].'" alt="...">
+                    <h3 class="titresite">'.$lignechrono1[$i]["titre_chrono1"].'</h3>
+                    <p>'.$lignechrono1[$i]["text_chrono1"].'</p>';
+                }
+                ?>
             </div>
-
-                <div class="col-4 test1"></div>
-
-            <div class="col-4">
-                <h3 class="titresite">Domaine et hébergement web</h3>
-                <p>Nous vous soutenons dans la recherche 
-                    de votre nom de domaine/d’hébergement 
-                    et dans l’enregistrement de celui-ci.</p>
-                <h3 class="titresite">Modules Optionnels</h3>
-                <p>Installation de modules optionnels : 
-                    Système de réservation, livre d’or, galerie…</p>
-                <h3 class="titresite">Intégration de contenu</h3>
-                <p>Envoyez-nous vos documents par mail 
-                    (textes, images, vidéos). Nous nous chargerons
-                    de corriger vos textes, de les structurer et 
-                    de les intégrer !</p>
-                <h3 class="titresite">Photos Professionnelles</h3>
-                <p>Nous vous offrons, pour chaque page, 
-                    une photographie professionnelle libre de droit.</p>
+                <div class="col-4 traitvertical">
+                    <div class="trait1"></div>
+                    <div class="trait2"></div>
+                    <div class="trait3"></div>
+                    <div class="trait4"></div>
+                    <div class="trait5"></div>
+                    <div class="trait6"></div>
+                    <div class="trait7"></div>
+                    <div class="trait8"></div>
+                </div>
+                <div class="col-lg-4 col-sm-12 text-center">
+                <?php $lignechrono2 = get_field("ligne_chronologique2");
+                    for ($i=0; $i < count($lignechrono2); $i++){
+                    echo '<img class="logochrono2" src="'.$lignechrono2[$i]["logo_chrono2"].'" alt="...">
+                    <h3 class="titresite">'.$lignechrono2[$i]["titre_chrono2"].'</h3>
+                    <p>'.$lignechrono2[$i]["text_chrono2"].'</p>';
+                }
+                ?>
             </div>
         </div>
     </div>
-        
-
-    <!-- <div class="main-timeline">
-
-                 
-                        <div class="timeline col-12">
-                            <div class="icon"></div>
-                            <div class="date-content">
-                                <div class="date-outer">
-                                    <span class="date">
-                                            <span class="month">2 Years</span>
-                                    <span class="year">2013</span>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="timeline-content">
-                                <h5 class="title">Visual Art &amp; Design</h5>
-                                <p class="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="timeline">
-                            <div class="date-content">
-                                <div class="date-outer">
-                                    <span class="date">
-                                            <span class="month">1 Years</span>
-                                    <span class="year">2015</span>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="timeline-content">
-                                <h5 class="title">Product Designer</h5>
-                                <p class="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="timeline">
-                            <div class="date-content">
-                                <div class="date-outer">
-                                    <span class="date">
-                                            <span class="month">2 Years</span>
-                                    <span class="year">2016</span>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="timeline-content">
-                                <h5 class="title">Web Designer</h5>
-                                <p class="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="timeline">
-                            <div class="date-content">
-                                <div class="date-outer">
-                                    <span class="date">
-                                            <span class="month">2 Years</span>
-                                    <span class="year">2018</span>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="timeline-content">
-                                <h5 class="title">Graphic Designer</h5>
-                                <p class="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
-                                </p>
-                            </div>
-                        </div>
-                        end experience section
-
-                    </div> -->
-  
 
 <?php get_footer(); ?>
